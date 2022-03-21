34151c0986d9576036f28d4285388d3054d42bdf
mynameislyonya <90193660+Lbabichev17@users.noreply.github.com>
2022-03-21 19:36:36 +0300

    Add files via upload

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -1,7 +1,8 @@
 # https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
 
+
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "fde48cf8bac364bf94bb3ef7d390b91bf8e50123bf96855b3742e0e8fb7ff9191f9d26decded3ac1a0bb0",
     "version": "5.126",
 }
